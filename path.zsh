96f9aaac5192cd977b5571b49a360bdff436c0dc
George Mamadashvili <georgemamadashvili@gmail.com>
2022-05-28 14:54:51 +0400

    GPG

--- path.zsh
+++ path.zsh
@@ -9,7 +9,9 @@ export PATH="/opt/homebrew/opt/openssl@1.1/bin:$PATH"
 # DBngin
 export PATH="/Users/Shared/DBngin/mysql/8.0.19/bin:$PATH"
 
+# GPG
 export GPG_TTY=$(tty)
+gpgconf --launch gpg-agent
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm

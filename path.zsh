a8d7e440efed243727ea14f79e49f1bf9a8486a8
George Mamadashvili <georgemamadashvili@gmail.com>
2026-06-26 10:49:31 +0400

    Fix DBngin path

--- path.zsh
+++ path.zsh
@@ -3,3 +3,7 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # PHP 8.3 (keg-only)
 export PATH="/opt/homebrew/opt/php@8.3/bin:/opt/homebrew/opt/php@8.3/sbin:$PATH"
+
+# DBngin MySQL client (so `wp db` can find the mysql binary)
+dbngin_mysql=(/Users/Shared/DBngin/mysql/*/bin(N))
+(( ${#dbngin_mysql} )) && export PATH="${dbngin_mysql[-1]}:$PATH"
